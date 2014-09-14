411bf10d0b28b152b73f494d735c2b917420202c
Renan Greca <renangreca@gmail.com>
2014-09-14 07:30:07 -0300

    added teste2.pas

---
+++ ProjetoBase/teste2.pas
@@ -0,0 +1,14 @@
+program comandoWhile (input, output):
+    var n, k: integer;
+        f1, f2, f3: integer;
+begin
+    read(n)
+    f1:=0; f2:=1; k:=1;
+    begin
+        f3:=f2+f1;
+        f1:=f2;
+        f2:=f3;
+        f:=k+1;
+    end;
+    write(n, k);
+end.
